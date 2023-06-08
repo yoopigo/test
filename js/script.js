45b9435703070d696d02f793656c914df8dcff42
yoopigo <collerggggggg@gmail.com>
2023-06-09 00:44:47 +0500

    test

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let intervalId;
 
 function switchAdvantages() {
-  const advantages = document.querySelectorAll('.advantage');
+  let advantages = document.querySelectorAll('.advantage');
   if (window.innerWidth <= 375) {
     let current = 0;
     intervalId = setInterval(() => {
@@ -24,3 +24,67 @@ window.onload = function () {
     switchAdvantages();
   });
 };
+
+///////////////////////
+
+let products = document.querySelectorAll('.reviews-grade');
+
+for (let i = 0; i < products.length; i++) {
+  let stars = products[i].querySelectorAll('.star');
+
+  stars.forEach((star, index) => {
+    star.addEventListener('click', () => {
+      for (let j = 0; j <= index; j++) {
+        stars[j].style.fill = '#FFDE00';
+      }
+      for (let j = index + 1; j < stars.length; j++) {
+        stars[j].style.fill = '#E0E0E0';
+      }
+    });
+  });
+}
+
+///////////////////////
+
+const goods = document.querySelectorAll('.bestsellers-good');
+const container = document.querySelector('.bestsellers-catalog');
+const leftArrow = document.querySelector('.arrow-left');
+const rightArrow = document.querySelector('.arrow-right');
+let currentIndex = 0;
+
+function showGoods() {
+  if (window.innerWidth <= 375) {
+    goods.forEach((good, index) => {
+      if (index === currentIndex) {
+        good.style.display = 'flex';
+      } else {
+        good.style.display = 'none';
+      }
+    });
+  } else {
+    goods.forEach((good) => {
+      good.style.display = 'flex';
+    });
+  }
+}
+
+showGoods();
+window.addEventListener('resize', showGoods);
+
+leftArrow.addEventListener('click', () => {
+  if (currentIndex === 0) {
+    currentIndex = goods.length - 1;
+  } else {
+    currentIndex--;
+  }
+  showGoods();
+});
+
+rightArrow.addEventListener('click', () => {
+  if (currentIndex === goods.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  showGoods();
+});
